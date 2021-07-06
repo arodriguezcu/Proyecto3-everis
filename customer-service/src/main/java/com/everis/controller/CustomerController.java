0ea1fdacff282541bf3283d911a5ae5b188e61d9
Alejandro <alejo_540@hotmail.com>
2021-07-06 14:18:25 -0500

    Update

--- customer-service/src/main/java/com/everis/controller/CustomerController.java
+++ customer-service/src/main/java/com/everis/controller/CustomerController.java
@@ -26,7 +26,7 @@ public class CustomerController {
   
   @Autowired
   private InterfaceCustomerService service;
-    
+  
   /** Listado de clientes. */
   @GetMapping
   public Mono<ResponseEntity<List<Customer>>> findAll() { 
