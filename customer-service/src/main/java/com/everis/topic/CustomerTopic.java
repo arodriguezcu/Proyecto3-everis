75abb143b538c2af43b496c5b96b9bc044d1b8bb
Alejandro <alejo_540@hotmail.com>
2021-07-07 21:31:44 -0500

    commit

--- customer-service/src/main/java/com/everis/topic/CustomerTopic.java
+++ customer-service/src/main/java/com/everis/topic/CustomerTopic.java
@@ -50,7 +50,7 @@ public class CustomerTopic {
 //    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, hostName + ":" + port);
   
 //    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
-    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "52.255.82.239:9092");
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "52.255.82.239:29092");
     
     config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
   
